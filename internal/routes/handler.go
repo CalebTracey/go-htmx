b57713cf40f4e35538c2aefc9fc7f4a84470792f
Caleb Tracey <caleb.tracey@gmail.com>
2024-02-04 23:36:36 -0500

    handler cleanup

--- internal/routes/handler.go
+++ internal/routes/handler.go
@@ -2,7 +2,11 @@ package routes
 
 import (
 	"errors"
+	"github.com/calebtracey/go-htmx/internal/common/pages"
 	"github.com/calebtraceyco/config"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+	"net/http"
 )
 
 type Handler struct {
@@ -14,6 +18,44 @@ func HandlerConfig(cfg *config.Config) (*Handler, error) {
 		return &Handler{Env: cfg.Env}, nil
 	}
 	return nil, nilConfigError
+}
+
+func navigateLanding(ctx echo.Context) error {
+	log.Infof("navigateLanding: rendering '%s'...", pages.Landing)
+	return ctx.Render(http.StatusOK, pages.Index, map[string]any{
+		"page":        "landing",
+		"description": "this is the landing page",
+		"message":     "hi mom",
+		"user":        "caleb.tracey",
+	})
+}
+
+func navigateHome(ctx echo.Context) error {
+	log.Infof("navigateHome: rendering '%s'...", pages.Home)
+	return ctx.Render(http.StatusOK, pages.Index, map[string]any{
+		"page":        "home",
+		"description": "this is the home page",
+		"message":     "stuff and things",
+		"user":        "caleb.tracey",
+	})
+}
+
+func navigateAbout(ctx echo.Context) error {
+	log.Infof("navigateAbout: rendering '%s'...", pages.About)
+	return ctx.Render(http.StatusOK, pages.Index, map[string]any{
+		"page":        "about",
+		"description": "this is the about section",
+		"message":     "lots of good stuff here",
+		"user":        "caleb.tracey",
+	})
+}
+
+func navigationHandler(fileName string, data map[string]any) echo.HandlerFunc {
+
+	return func(ctx echo.Context) error {
+		log.Infof("templateHandler: rendering '%s'...", fileName)
+		return ctx.Render(http.StatusOK, pages.Index, data)
+	}
 }
 
 var nilConfigError = errors.New("nil config")
--- internal/routes/initialize.go
+++ internal/routes/initialize.go
@@ -6,7 +6,6 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
-	"net/http"
 )
 
 func (h Handler) Initialize() *echo.Echo {
@@ -33,42 +32,4 @@ func (h Handler) Initialize() *echo.Echo {
 	e.GET("/about", navigateAbout)
 
 	return e
-}
-
-func navigateLanding(ctx echo.Context) error {
-	log.Infof("navigateLanding: rendering '%s'...", pages.Landing)
-	return ctx.Render(http.StatusOK, pages.Index, map[string]any{
-		"page":        "landing",
-		"description": "this is the landing page",
-		"message":     "hi mom",
-		"user":        "caleb.tracey",
-	})
-}
-
-func navigateHome(ctx echo.Context) error {
-	log.Infof("navigateHome: rendering '%s'...", pages.Home)
-	return ctx.Render(http.StatusOK, pages.Index, map[string]any{
-		"page":        "home",
-		"description": "this is the home page",
-		"message":     "stuff and things",
-		"user":        "caleb.tracey",
-	})
-}
-
-func navigateAbout(ctx echo.Context) error {
-	log.Infof("navigateAbout: rendering '%s'...", pages.About)
-	return ctx.Render(http.StatusOK, pages.Index, map[string]any{
-		"page":        "about",
-		"description": "this is the about section",
-		"message":     "lots of good stuff here",
-		"user":        "caleb.tracey",
-	})
-}
-
-func navigationHandler(fileName string, data map[string]any) echo.HandlerFunc {
-
-	return func(ctx echo.Context) error {
-		log.Infof("templateHandler: rendering '%s'...", fileName)
-		return ctx.Render(http.StatusOK, pages.Index, data)
-	}
 }
